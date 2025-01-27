ffcb00fbc344b5c2334382ff9171ac2c13a4fa87
Namoos99 <Namoos.Haider@du.edu>
2025-01-27 15:51:21 -0700

    git commit -m "Add random number generator script"

---
+++ import random.py
@@ -0,0 +1,9 @@
+import random
+
+def main():
+    # Generate one random number between 1 and 100
+    random_number = random.randint(1, 100)
+    print("Your random number is:", random_number)
+
+if __name__ == "__main__":
+    main()
