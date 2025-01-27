c5baadd8974109053cda21b723a1255bdc08b089
Namoos99 <Namoos.Haider@DU.edu>
2025-01-27 14:39:19 -0700

    Add files via upload

---
+++ random-number-generator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import random
+
+def main():
+    # Generate one random number between 1 and 100
+    random_number = random.randint(1, 100)
+    print("Your random number is:", random_number)
+
+if __name__ == "__main__":
+    main()
